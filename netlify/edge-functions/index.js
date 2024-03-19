7517c46d85c3f466f3a3eeebbb680d70180a70e7
VargaElod23 <vargaelod23@gmail.com>
2024-03-19 13:28:54 +0100

    add access-control-allow-origin to headers

--- netlify/edge-functions/index.js
+++ netlify/edge-functions/index.js
@@ -3,7 +3,8 @@ export default async (request, context) => {
   return new Response(response.body, {
     headers: {
       "access-control-allow-origin": "*",
-      "access-control-allow-headers": "x-request-source, content-type",
+      "access-control-allow-headers":
+        "x-request-source, content-type, access-control-allow-origin",
     },
   });
 };
